64062f6a442340470096f4e40313dcbb5496979d
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-11-06 10:58:34 +0200

    add common styles

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//Import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Import Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+//Import Boxicons
+import 'boxicons/css/boxicons.min.css';
+
+import './App.css'
+import Banner from 'pages/Banner';
+
+export const App = () => {
+  return (
+    <div>
+      <Banner/>
+    </div>
+  );
+};
