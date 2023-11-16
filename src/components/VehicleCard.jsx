33b0cd8451024e81785519c98055b97d20bd6706
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-11-16 10:35:50 +0200

    start VehicleCard

---
+++ src/components/VehicleCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './vehicleCard.css';
+
+const VehicleCard = () => {
+  return (
+    <div className='col-lg-4 col-md-6'></div>
+  )
+}
+
+export default VehicleCard
--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -12,7 +12,11 @@ const Vehicles = () => {
     <div id='vehicles' className='page vehicles' >
       <img src={bgImg} alt="" className='img-fluid page-img' />
       <div className="container">
-        <PageTitle title="Vehicles Catalogue" subtitle="Here are our new arrivals"/>
+        <PageTitle title="Vehicles Catalogue" subtitle="Here are our new arrivals" />
+        <div className="row">
+          {cars && cars.length > 0 && cars.map(car => <h1 key={car._id}>{car.title}</h1>
+          )}
+        </div>
       </div>
 
     </div>
