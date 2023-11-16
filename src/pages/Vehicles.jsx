78c24edc9ad923db853646b2ba26209f1eaab8e4
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-11-16 10:26:05 +0200

    add PageTitle component on Vehicles page

--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import './vehicles.css'
+import React, { useContext } from 'react';
+import './vehicles.css';
+
+import bgImg from '../images/mclaren-1.jpg';
+import { AppContext } from 'App';
+import PageTitle from 'components/PageTitle';
 
 const Vehicles = () => {
+  const { data: cars, setData: setCars } = useContext(AppContext);
+
   return (
-    <div>Vehicles</div>
+    <div id='vehicles' className='page vehicles' >
+      <img src={bgImg} alt="" className='img-fluid page-img' />
+      <div className="container">
+        <PageTitle title="Vehicles Catalogue" subtitle="Here are our new arrivals"/>
+      </div>
+
+    </div>
   )
 }
 
