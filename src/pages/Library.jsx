b8bed5d574a4fc8e15cdc0c5144c8af5604c1495
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-11-21 10:37:31 +0200

    finish Library markup

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -3,6 +3,8 @@ import './library.css'
 import bgImg from '../images/mclaren-1.jpg';
 import PageTitle from 'components/PageTitle';
 import { AppContext } from 'App';
+import { Link } from 'react-router-dom';
+import VehicleCard from 'components/VehicleCard';
 
 const Library = () => {
   const {library: cars} = useContext(AppContext)
@@ -12,7 +14,19 @@ const Library = () => {
       <div className="container">
         <PageTitle
           title='Vehicles Library' subtitle='Yere are your favourites' />
-        <div className="row"></div>
+        <div className="row">
+          {
+            cars && cars.length === 0 ? (
+              <div className="text-center">
+                <h3 className="empty">You have no collections</h3>
+                <Link className='browse' to='/vehicles'>
+                Browse Vehicles</Link>
+              </div>
+            ) : (
+                cars.map(car => <VehicleCard key={car._id} car={car} />)
+            )
+           }
+        </div>
       </div>
     </div>
   )
