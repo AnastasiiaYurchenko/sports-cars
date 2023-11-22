eba0b3f487cf624c60a635aaba796b2aac3587d5
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-11-22 23:17:50 +0200

    add useCallback in RatingStars

--- src/components/VehicleRating.jsx
+++ src/components/VehicleRating.jsx
@@ -1,22 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import './vehicleRating.css'
 
 const VehicleRating = ({ rating }) => {
     const [stars, setStars] = useState([]);
-    const generateStars = () => {
-        let stars = [];
-        if (rating > 5 || rating < 1) {
-            return
-        }
-        for (let i = 0; i < rating; i++) {
-            stars.push(i)
-        }
-        return stars;
-    }
 
-    useEffect(() => {
-        setStars(generateStars())
-    }, [])
+    // const generateStars = () => {
+    //     let stars = [];
+    //     if (rating > 5 || rating < 1) {
+    //         return
+    //     }
+    //     for (let i = 0; i < rating; i++) {
+    //         stars.push(i)
+    //     }
+    //     return stars;
+    // }
+
+    // useEffect(() => {
+    //     setStars(generateStars())
+    // }, [])
+
+    const generateStars = useCallback(() => {
+    let stars = [];
+    if (rating > 5 || rating < 1) {
+        return;
+    }
+    for (let i = 0; i < rating; i++) {
+        stars.push(i);
+    }
+    return stars;
+}, [rating]);
+
+useEffect(() => {
+    setStars(generateStars());
+}, [generateStars, rating]);
+
 
   return (
       <div className='stars'> { stars.map(star => (<i key={star} class="bi bi-star-fill"></i>))}</div>
